85166ec31576225783c50b60ac2b1992b6454de0
Ace <acequantumbit@gmail.com>
2023-09-21 18:00:46 -0700

    added script to print modified alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * putchar - prints out a single character
+ *
+ * Return: return a 0 upon proper execution
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' || letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
