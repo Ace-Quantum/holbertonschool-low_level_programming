76befdfd085894cac8d27df9e0ab20aba75f861d
Ace <acequantumbit@gmail.com>
2023-09-21 17:37:59 -0700

    made script to print the alphabet twice

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * putchar - prints out a single character
+ *
+ * Return: return a 0 upon proper execution
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	char better = 'A';
+
+	while (better <= 'Z')
+	{
+		putchar(better);
+		better++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
