28db2efacce09213c3d05edb1b74c7e7b58cd892
Ace <acequantumbit@gmail.com>
2023-09-21 18:26:45 -0700

    made script to print hexidecimals

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * putchar - prints out a single character
+ *
+ * Return: return a 0 upon proper execution
+ */
+
+int main(void)
+{
+	int number = '0';
+	char letter = 'a';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
