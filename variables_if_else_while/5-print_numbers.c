9b3da8aeeca7388c3778ef8439e1b2fc004a2265
Ace <acequantumbit@gmail.com>
2023-09-21 18:08:05 -0700

    made script to print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * putchar - prints out a single character
+ *
+ * Return: return a 0 upon proper execution
+ */
+
+int main(void)
+{
+	printf("0123456789");
+
+	return (0);
+}
