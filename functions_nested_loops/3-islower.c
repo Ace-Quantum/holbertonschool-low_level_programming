00cf14f7fc4e3d48835fd9d2a0f3195154bd5bae
Ace <acequantumbit@gmail.com>
2023-09-22 18:06:06 -0700

    made script to determine if a variable is uppercase or lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Where the program starts
+ *
+ * Return: return a 0 upon letter being uppercase, 1 if lowercase
+ *
+ * c: character to be tested
+ *
+ * Description: prints out "_putchar"
+ */
+
+int _islower(int c)
+{
+	if (c > 64 && c < 91)
+		return (0);
+	else if (c > 96 && c < 123)
+		return (1);
+}
