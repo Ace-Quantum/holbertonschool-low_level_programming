bf65e551dc12a23c55c333aaf6ed04f223b34349
Ace <acequantumbit@gmail.com>
2023-09-22 17:26:04 -0700

    adding a script to repete the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_X10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_X10 - Program start
+ *
+ * Return: void
+ */
+
+void print_alphabet_X10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
