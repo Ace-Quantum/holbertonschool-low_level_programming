eb5c8ef31d0885570ecc2498299994782321e339
Ace <acequantumbit@gmail.com>
2023-09-23 17:34:01 -0700

    made script to return the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Where the program starts
+ *
+ * Return: return a upon proper execution
+ *
+ * @n: a number
+ *
+ * Description: prints out "_putchar"
+ */
+
+int print_last_digit(int n)
+{
+	a = n % 10;
+
+	return (a);
+}
