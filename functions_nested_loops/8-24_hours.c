3842222bf832699b92131fe684243b43e9b14a46
Ace <acequantumbit@gmail.com>
2023-09-24 17:40:45 -0700

    bug fix, necrophiled from the code graveyard

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
-/**
- * print_alphabet_x10 - Program start
- *
- * Return: void
- */
-
-void print_digit(int n)
-{
-	if (n < 10)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
-}
-
-void jack_bauer(void)
+void jack_bauer(void);
 {
 	int h = 0;
 	int m = 0;
@@ -27,13 +9,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60 ; m++)
 		{
-			print_digit(h / 10);
-			print_digit(h % 10);
+			_putchar(h / 10);
+			_putchar(h % 10);
 			_putchar(':');
-			print_digit(m / 10);
-			print_digit(m % 10);
+			_putchar(m / 10);
+			_putchar(m % 10);
 			_putchar('\n');
 		}
 	}
 }
-
