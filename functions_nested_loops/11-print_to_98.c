e0c97e2f66750fdecd099ed863d08415149a344f
Ace <acequantumbit@gmail.com>
2023-09-25 00:59:44 -0700

    bug fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		{
 			printf ("%d", n);
 			printf (", ");
-			n == (n + 1);
+			n = (n + 1);
 		}
 		printf ("98");
 	}
@@ -27,7 +27,7 @@ void print_to_98(int n)
 		{
 			printf ("%d", n);
 			printf (", ");
-			n == (n - 1);
+			n = (n - 1);
 		}
 		printf (98);
 	}
