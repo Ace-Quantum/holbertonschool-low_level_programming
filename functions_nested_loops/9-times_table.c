05fd9844dab2786c2c1b27a1bbeb51d507dcb449
Ace <acequantumbit@gmail.com>
2023-09-24 20:56:15 -0700

    bug fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,12 +10,22 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10 ; i++)
 	{
 		for (j = 0; j < 10 ; j++)
 		{
-			_putchar((i + '0') * (j + '0'));
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
 			if (j != 9)
 			{
 				_putchar(',');
