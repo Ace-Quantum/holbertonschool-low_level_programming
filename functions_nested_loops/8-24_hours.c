58ed448a56aae9491c2627965814189bc455f57c
Ace <acequantumbit@gmail.com>
2023-09-24 18:09:59 -0700

    I give up

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,48 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Program start
+ *
+ * Return: void
+ */
+
 void jack_bauer(void)
 {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
 	int h = 0;
 	int m = 0;
 
-	for (h = 0; h < 24 ; h++)
+	while (m < 1441)
 	{
-		for (m = 0; m < 60 ; m++)
+		while (a < 3)
 		{
-			_putchar(h / 10);
-			_putchar(h % 10);
-			_putchar(':');
-			_putchar(m / 10);
-			_putchar(m % 10);
-			_putchar('\n');
+			while (b < 10)
+			{
+				while (c < 6)
+				{
+					while (d < 10)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+						m = (m + 1);
+						d = (d + 1);
+					}
+					d = 0;
+					c = (c + 1);
+				}
+				c = 0;
+				b = (b + 1);
+			}
+			b = 0;
+			a = (a + 1);
 		}
 	}
 }
+
