86802003f88abab50f42414b3535bb320fe44c2c
Ace <acequantumbit@gmail.com>
2023-09-24 20:46:14 -0700

    made script to print times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,3 +8,20 @@
 
 void times_table(void)
 {
+	int i;
+	int j;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			_putchar(i * j);
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
