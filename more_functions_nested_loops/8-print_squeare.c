0cb6fa54dafda0b22faaf5597500f2e693f61571
Ace <acequantumbit@gmail.com>
2023-09-28 12:39:54 -0700

    made script to print squares

---
+++ more_functions_nested_loops/8-print_squeare.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - starting point of the script, which prints a square
+ *
+ * @size: determines the width of said square
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	while i < size
+	{
+		while j < size
+		{
+			_putchar('#');
+			j++;
+		}
+		j = 0;
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}	
