55d3e7d1d7e10dd4eee2d2dca78ac1df9dab1bea
Ace <acequantumbit@gmail.com>
2023-09-28 11:40:54 -0700

    Made script to print a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Start of a script to print a line
+ *
+ * @n: determines how many lines to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i > 1 ; i++)
+		_putchar("_");
+	_putchar('\n');
+}
