656d979ad37451ba3ba479cbde242473b194465f
Ace <acequantumbit@gmail.com>
2023-09-28 13:27:08 -0700

    FIZZBUSSIN

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * FizzBuzz - FIZZBUZZFIZZBUZZFIZZBUZZ
+ */
+
+void FizzBuzz(void)
+{
+	int i = 0;
+
+	while (i < 101)
+	{
+		if (i % 15)
+			printf("FizzBuzz");
+		else if (i % 3)
+			printf("Fizz");
+		else if (i % 5)
+			printf("Buzz");
+		else
+			printf(i + '0');
+		putchar(' ');
+		i++
+	}
+}
