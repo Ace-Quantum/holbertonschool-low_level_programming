de70dea4b17a841e47684370e9541ff0930bb2ec
Ace <acequantumbit@gmail.com>
2023-09-28 10:11:04 -0700

    bug fix

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,15 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < 15 ; i++)
+	for (j = 0; j < 10 ; j++)
 	{
-		if (i > 9)
-			_putchar((i / 10) + '0');
-		_putchar(i % 10);
-		_putchar('\n');
-	}
+		for (i = 0; i < 15 ; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n')
 }
