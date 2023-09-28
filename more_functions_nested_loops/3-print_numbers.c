eac90536151802c3f636be108abab1bb4828bc65
Ace <acequantumbit@gmail.com>
2023-09-28 09:14:27 -0700

    made program to print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - starting point, prints numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i)
+		i++
+	}
+
+	_putchar('\n')
+}
