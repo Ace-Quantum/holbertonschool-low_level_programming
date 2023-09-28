2a78f89ad3c856adb98b0b2311ec8217da2b12bd
Ace <acequantumbit@gmail.com>
2023-09-28 09:07:39 -0700

    made script for multiplying numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - program start
+ *
+ * Return: two numbers multiplied together
+ *
+ * @a: a number to be multiplied
+ *
+ * @b: a number to be multiplied
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return(c)
+}
