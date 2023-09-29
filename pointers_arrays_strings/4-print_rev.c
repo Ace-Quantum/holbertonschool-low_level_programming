50a87fbd0966073359b359c1c201af211c761051
Ace <acequantumbit@gmail.com>
2023-09-29 10:02:10 -0700

    Made script to print things out backwards

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - starting point of the code
+ *
+ * @s: string to be reverse printed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = _strlen(*s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
