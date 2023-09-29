1addd8c69dbea69142a71004bdee7acd8d16db7a
Ace <acequantumbit@gmail.com>
2023-09-29 09:50:29 -0700

    made script to print out a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - start of the program, prints out a string
+ *
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+}
