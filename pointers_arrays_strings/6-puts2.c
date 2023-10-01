c5611f7d6a0c4c6b90cb0a88c685bfb2b56b96bc
Ace <acequantumbit@gmail.com>
2023-09-30 17:49:19 -0700

    bug fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - start of the file, prints every other character
+ * puts2 - starting point of the file
  *
  * @str: the str to be skipped through
  */
@@ -10,10 +10,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && str[i + 1] != '\0')
+	while (str[i] != 0)
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i = i + 1;
 	}
 	_putchar('\n');
 }
