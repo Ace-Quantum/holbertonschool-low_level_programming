afc47d449f93e9f0094db71e910b941dc8f29db1
Ace <acequantumbit@gmail.com>
2023-09-30 18:23:13 -0700

    Made script to loop throught the elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - starting point of printing a certain number of values
+ *
+ * @a: array to be printed
+ *
+ * @n: amount of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+	while (a[n] != 0)
+	{
+		_putchar(a[n]);
+		if (a[n + 1] == 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
+	}
+	_putchar('\n');
