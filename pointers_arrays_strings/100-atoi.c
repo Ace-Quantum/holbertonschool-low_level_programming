99474afe6d66bd99a3cb7ea024021188805fc193
Ace <acequantumbit@gmail.com>
2023-10-01 21:37:19 -0700

    bug fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,32 +6,32 @@
 
 int _atoi(char *s)
 {
-	int GenMachine;
-	int Gen;
+	int PosNegDet;
+	int PosNeg;
 	int Digit;
 	int Result;
 
-	GenMachine = 0;
+	PosNegDet = 0;
 	Result = 0;
 
 	while (*s != '\0')
 	{
 		if (*s == 43)
-			GenMachine = GenMachine + 1;
+			PosNegDet = PosNegDet + 1;
 		else if (*s == 45)
-			GenMachine = GenMachine - 1;
+			PosNegDet = PosNegDet - 1;
 		else if (*s > 47 && *s < 58)
 		{
-			Digit = *s + '0';
+			Digit = *s - '0';
 			Result = Result * 10 + Digit;
 		}
 		s++;
 	}
 
-	if (GenMachine >= 0)
-		Gen = 1;
+	if (PosNegDet >= 0)
+		PosNeg = 1;
 	else
-		Gen = -1;
+		PosNeg = -1;
 
-	return Gen * Result;
+	return PosNeg * Result;
 }
