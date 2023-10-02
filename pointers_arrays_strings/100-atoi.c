f734a928a7063375bd7908a374f7ea25827069f1
Ace <acequantumbit@gmail.com>
2023-10-01 21:02:33 -0700

    Let's see if this works

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * 
+ */
+
+int _atoi(char *s)
+{
+	int GenMachine;
+	int Gen;
+	int Digit;
+	int Result;
+
+	GenMachine = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == 43)
+			GenMachine = GenMachine + 1;
+		else if (*s == 45)
+			GenMachine = GenMachine - 1;
+		else if (*s > 47 && *s < 58)
+		{
+			Digit = *s + '0';
+			Result = Result * 10 + Digit;
+		}
+		s++;
+	}
+
+	if (GenMachine >= 0)
+		Gen = 1;
+	else
+		Gen = -1;
+
+	return Gen * Result;
+}
