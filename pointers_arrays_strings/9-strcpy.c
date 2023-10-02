b9774be2b549de3a5ff398f1f07a352a00ed5daa
Ace <acequantumbit@gmail.com>
2023-10-01 18:35:04 -0700

    appeasing betty

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
 /**
+ * *_strcpy - starting point of the prototype
  *
+ * @dest: The pointer to which src will be copied.
+ *
+ * @src: the string to be copied
+ *
+ * Return: The starting pointer for dest
  */
 
 char *_strcpy(char *dest, char *src)
