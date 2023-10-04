2fc4d08ff35e37e7b5d52b1fca1c4a170c9bb1be
Ace <acequantumbit@gmail.com>
2023-10-04 11:39:49 -0700

    made code to concat strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,22 @@
 #include "main.h"
 
 /**
+ * _strcat - concatinates strings
  *
+ * @dest: beginning of string
+ * @src: the end of string
+ *
+ * Return: return *dest_start
  */
 
+char *_strcat(char *dest, char *src)
+{
+	char *dest_start = dest;
 
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+		*dest++ = *src++;
+	*dest = '\0';
+	return (*dest_start);
+}
