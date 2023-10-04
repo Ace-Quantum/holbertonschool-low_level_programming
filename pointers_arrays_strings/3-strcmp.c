7545929c84cb8a89b5d1f0fdb78d2a56fa72f78d
Ace <acequantumbit@gmail.com>
2023-10-04 16:38:05 -0700

    I don't understand

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = _strlen(s1);
-	int len2 = _strlen(s2);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	/*if (len1 > len2)
-		return (1);
-	else if (len2 > len1)
-		return (-1);*/
-	if (len1 == len2)
-		return (0);
-	else
-		return (len1 - len2);
+	return (*s1 - *s2);
 }
