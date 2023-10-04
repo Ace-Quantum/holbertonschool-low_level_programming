d70d7e0a1cb6d10af1b505d4f2eb5ea3e9077d8a
Ace <acequantumbit@gmail.com>
2023-10-04 11:43:13 -0700

    bug fix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -27,7 +27,7 @@ int _atoi(char *s);
 
 char *_strcat(char *dest, char *src);
 
-char *strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
