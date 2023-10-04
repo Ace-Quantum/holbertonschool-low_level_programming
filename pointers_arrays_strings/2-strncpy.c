3e804368284959dba5b48c26bf2b4f6378c0ac90
Ace <acequantumbit@gmail.com>
2023-10-04 14:35:28 -0700

    bug fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,14 @@
-#include "main"
+#include "main.h"
 #include "2-strlen.c"
 
 /**
+ * _strncpy - start of the program
  *
+ * @dest: where the string will be copied to
+ * @src: the string to be copied
+ * @n: the extent of copying to be done
+ *
+ * Return: return the slab
  */
 
 char *_strncpy(char *dest, char *src, int n)
