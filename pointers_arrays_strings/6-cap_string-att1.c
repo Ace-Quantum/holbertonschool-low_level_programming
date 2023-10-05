8fca9f39e252cd32d7b41af3c79024ab663aa51d
Ace <acequantumbit@gmail.com>
2023-10-05 12:00:06 -0700

    changed loops for 6-cap

---
+++ pointers_arrays_strings/6-cap_string-att1.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	char *s = str;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 96 && str[i] < 123) || (str[i] > 64 && str[i] < 91))
+		{
+			if (str[i] > 96 && str[i] < 123)
+				str[i] = str[i] - 32;
+			while ((str[i] > 96 && str [i] <123) || (str[i] > 64 && str[i] < 91))
+				i++;
+		}
+		i++;
+	}
+	return (s);
+}
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
-/**
- *
- */
-
 char *cap_string(char *str)
 {
+	int sep[11] = {44, 59, 46, 33, 63, 34, 40, 41, 32, 9, 10};
 	int i = 0;
 	char *s = str;
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] > 96 && str[i] < 123) || (str[i] > 64 && str[i] < 91))
+		for (j = 0; j < 12; j++)
 		{
-			if (str[i] > 96 && str[i] < 123)
-				str[i] = str[i] - 32;
-			while ((str[i] > 96 && str [i] <123) || (str[i] > 64 && str[i] < 91))
-				i++;
+			if (str[i] = sep[j])
+			{
+				if (str[i + 1] > 96 && str[i] < 123)
+					str[i + 1] = str[i + 1] - 32;
+			}
 		}
 		i++;
 	}
-	return (s);
 }
