5cd4d5422025bfe538531e33dc5e1e1252baab8c
Ace <acequantumbit@gmail.com>
2023-10-05 16:46:52 -0700

    lberton cat

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory to another place of memory
+ *
+ * @dest: where the memory gets copied to
+ * @src: string to copy
+ * @n: Amount to copy
+ *
+ * Return: return the start of the copied string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *s = dest;
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest(i) = src(i);
+		i++;
+	}
+
+	return (s);
+}
