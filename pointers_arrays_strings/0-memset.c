666c760dbb9168ebe88129d79d4ee2d4da36b8e6
Ace <acequantumbit@gmail.com>
2023-10-05 16:05:57 -0700

    Bug fix

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,12 +5,14 @@
  */
 
 char *_memset(char *s, char b, unsigned int n);
-
-int i = 0;
-char *r = s;
-
-while (i <= n)
 {
-	s[i] = b;
-	i++;
+	int i = 0;
+	char *r = *s;
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (r);
 }
