c43cad534bf0700eeccaa89123dd42f1f229f9ae
Ace <acequantumbit@gmail.com>
2023-10-05 16:19:23 -0700

    appeasing betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
 /**
+ * _memset - start of code to reassign memory
  *
+ * @s: pointer to the code to be rewritten
+ * @b: character to rewrite
+ * @n: amount of memory to go through
+ *
+ * Return: the begining of the string
  */
 
 char *_memset(char *s, char b, unsigned int n)
