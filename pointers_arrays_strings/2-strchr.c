97b36409afba78bb9df6b75869cac558e1ac4b5d
Ace <acequantumbit@gmail.com>
2023-10-05 17:22:00 -0700

    bug fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,13 @@ char *_strchr(char *s, char c)
 	char *r;
 	int i = 0;
 
-	while (s[i] != c)
+	while (s[i] != c && s[i] != 0)
 		i++;
 
-	r = &s[i];
+	if (s[i] != 0)
+		r = &s[i];
+	else
+		r = 0;
+
 	return (r);
 }
