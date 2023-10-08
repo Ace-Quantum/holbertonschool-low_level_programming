ebd5f873476ea65130c0835bf6c393d3ad88c5e5
Ace <acequantumbit@gmail.com>
2023-10-08 11:55:56 -0700

    bug fix

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -13,14 +13,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	char *r = '\0';
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 	int lens = _strlen(s);
 	int lena = _strlen(accept);
 
-	for (i < lens; i++)
+	for (i = 0; i < lens; i++)
 	{
-		for (j < lena; j++)
+		for (j = 0; j < lena; j++)
 		{
 			if (s[i] == accept[j])
 			{
