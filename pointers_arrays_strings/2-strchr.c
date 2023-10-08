2ff706c6852dbae56fac43f9c58e27f80e3b1efd
Ace <acequantumbit@gmail.com>
2023-10-07 17:34:35 -0700

    90% is fine

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
 /**
+ * _strchr - starting point of the program
  *
+ * @s: pointer to the string in question
+ * @c: character to locate
+ *
+ * Return: pointer to the character that has been located
  */
 
 char *_strchr(char *s, char c)
