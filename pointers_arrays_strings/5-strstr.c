bcd6bbc039cc12f41b9910b06453bb8881a2f375
Ace <acequantumbit@gmail.com>
2023-10-08 13:55:49 -0700

    made some good looking code

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int lenha = _strlen(haystack);
+	int lenne = strlen(needle);
+	char *r;
+
+	while (i < lenha)
+	{
+		if (haystack[i] == needle[j])
+		{
+			r = &haystack[i];
+			while (j < lenne)
+			{
+				if (haystack[i] == needle[j])
+				{
+					if (j = lenne)
+						return (r);
+					i++;
+					j++;
+				}
+				else
+				{
+					r = '\0';
+					j = 0;
+					break;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (r);
+}
