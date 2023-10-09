98abf411a8624570e620448bf9fe3e17329fa647
Ace <acequantumbit@gmail.com>
2023-10-08 18:37:02 -0700

    bug fix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	int arr[10][10];
 	int total = (size * size);
 
-	while (i <= total)
+	while (i < total)
 	{
 		for (c = 0; c < size; c++)
 		{
@@ -31,17 +31,17 @@ void print_diagsums(int *a, int size)
 
 	i = 0;
 
-	while (i <= size)
+	while (i < size)
 	{
 		sum1 = sum1 + arr[i][j];
 		i++;
 		j++;
 	}
 
-	i = size;
+	i = size - 1;
 	j = 0;
 
-	while (j <= size)
+	while (j < size)
 	{
 		sum2 = sum2 + arr[i][j];
 		i--;
