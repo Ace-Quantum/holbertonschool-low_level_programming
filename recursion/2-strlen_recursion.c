00469ddb9e6775614735150c6b1caa8f8abeeb60
Ace <acequantumbit@gmail.com>
2023-10-11 16:20:04 -0700

    made a recursive loop to return a string length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	RecursiveStrLen(s);
+	return(x)
+}
+
+int RecursiveStrLen(char *s)
+{
+	if (s[x] == '\0')
+		return(x);
+
+	x++;
+	RecursiveStrLen(s);
+}
