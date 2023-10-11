690c99c3459f40f5d317af0f93e32eddf3c40a51
Ace <acequantumbit@gmail.com>
2023-10-11 11:50:43 -0700

    built first recursive function

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_strlen.c"
+
+/**
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	int x = 0;
+
+	_putchar(s[x]);
+	x--;
+	if (x < _strlen(x) - 1)
+		_puts_recursion(*s);
+}
---
+++ recursion/README.md
@@ -0,0 +1,3 @@
+This is a ReadMe. I'm supposed to update it with what I do
+
+I most likely will not be doing that.
