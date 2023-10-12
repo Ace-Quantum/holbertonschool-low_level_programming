c8d3cedd80c4483f8415fda59191c9f82bff29a4
Ace <acequantumbit@gmail.com>
2023-10-12 15:22:17 -0700

    bug fix

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	int i = 1;
+
+	return (rec_prime(n, i));
+}
+
+int rec_prime(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+
+	if (i > n / 2)
+		return (1);
+
+	return (rec_prime(n, i - 1));
+}
