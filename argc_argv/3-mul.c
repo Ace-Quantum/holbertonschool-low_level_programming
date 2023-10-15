65d0d63db4bf01bbbd88706e78b2f6961eb52087
Ace <acequantumbit@gmail.com>
2023-10-15 16:04:50 -0700

    bug fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
-{
-	if (argc != 2)
-	{
-		printf("Error");
-		printf("\n");
-		return (1);
-	}
-
-	mult_num(argc, argv);
-	return (0);
-}
-
 void mult_num(int argc, char* argv[])
 {
 	int x = (c[1] * c[2]);
 
 	printf("%d", x);
+}
+
+int main(int argc, char* argv[])
+{
+        if (argc != 2)
+        {
+                printf("Error");
+                printf("\n");
+                return (1);
+        }
+
+        mult_num(argc, argv);
+        return (0);
 }
