545fd91592e07399d672dff32282668444cf22c9
Ace <acequantumbit@gmail.com>
2023-10-15 16:36:49 -0700

    bug fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,8 +2,8 @@
 
 void mult_num(int argc, char* argv[])
 {
-	int mult1 = atoi(argv[1]);
-	int mult2 = atoi(argv[2]);
+	int mult1 = atoi(*argv[1]);
+	int mult2 = atoi(*argv[2]);
 
 	int x = (mult1 * mult2);
 
