812c6c1bcb105d0b56f8f28e0c77f6ce17f49713
Ace <acequantumbit@gmail.com>
2023-10-15 17:11:30 -0700

    trying something new

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mult_num(int argc, char* argv[])
-{
-	void(argc);
-
-	int mult1 = atoi(argv[1]);
-	int mult2 = atoi(argv[2]);
-
-	int x = (mult1 * mult2);
-
-	printf("%d", x);
-}
-
 int main(int argc, char* argv[])
 {
-        if (argc != 3)
+        int mult1 = atoi(argv[1]);
+	int mult2 = atoi(argv[2]);
+	
+	if (argc != 3)
         {
                 printf("Error");
                 printf("\n");
                 return (1);
         }
 
-        mult_num(argc, argv);
+        printf("%d\n", (mult1 * mult2));
         return (0);
 }
