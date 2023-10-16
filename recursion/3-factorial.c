7ec9e4192680474b161d5415052c375e1b0caced
Ace <acequantumbit@gmail.com>
2023-10-15 19:34:46 -0700

    appeasing betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,14 +1,27 @@
 #include "main.h"
 
+/**
+ * factorial - start of the program
+ * @n: the number to factorialize
+ * Return: the number but factorialized
+ */
+
 int factorial(int n)
 {
 	int x = 1;
 
 	if (n < 0)
-		return(-1);
+		return (-1);
 
 	return (factorio(n, x));
 }
+
+/**
+ * factorio - does the factorializing
+ * @n: the number to factorialize
+ * @x: the counter for how many times you've factorialized
+ * Return: the factorialized number
+ */
 
 int factorio(int n, int x)
 {
