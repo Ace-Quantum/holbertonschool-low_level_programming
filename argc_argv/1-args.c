1aa86e328252e3687fad9d031206f54caaf19884
Ace <acequantumbit@gmail.com>
2023-10-15 20:59:54 -0700

    appeasing betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
+/**
+ * print_arg_int - start of program
+ * @argc: number of arguments
+ */
+
 void print_arg_int(int argc)
 {
-	if (argc >=1)
+	if (argc >= 1)
 		printf("%d\n", argc - 1);
 	else
 		printf("error");
 }
+
+/**
+ * main - start of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 upon success
+ */
 
 int main(int argc, char *argv[])
 {
