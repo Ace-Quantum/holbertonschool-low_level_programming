30852411ce28c2e866da9d1e937b358a8825fb6f
Ace <acequantumbit@gmail.com>
2023-10-15 21:08:07 -0700

    appeasing betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - start of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 upon success
+ */
+
+int main(int argc, char *argv[])
 {
 	int mult1;
 	int mult2;
 
 	if (argc != 3)
-        {
-                printf("Error");
-                printf("\n");
-                return (1);
-        }
+	{
+		printf("Error");
+		printf("\n");
+		return (1);
+	}
 
 	mult1 = atoi(argv[1]);
-        mult2 = atoi(argv[2]);
+	mult2 = atoi(argv[2]);
 
 	printf("%d\n", (mult1 * mult2));
-        return (0);
+	return (0);
 }
