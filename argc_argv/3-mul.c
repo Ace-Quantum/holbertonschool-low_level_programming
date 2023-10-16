a7c1b46f568f8358fe0c9be370950264e3e4ca0a
Ace <acequantumbit@gmail.com>
2023-10-15 17:37:30 -0700

    bug fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,9 +3,6 @@
 
 int main(int argc, char* argv[])
 {
-        int mult1 = atoi(argv[1]);
-	int mult2 = atoi(argv[2]);
-	
 	if (argc != 3)
         {
                 printf("Error");
@@ -13,6 +10,9 @@ int main(int argc, char* argv[])
                 return (1);
         }
 
-        printf("%d\n", (mult1 * mult2));
+	int mult1 = atoi(argv[0]);
+        int mult2 = atoi(argv[2]);
+
+	printf("%d\n", (mult1 * mult2));
         return (0);
 }
