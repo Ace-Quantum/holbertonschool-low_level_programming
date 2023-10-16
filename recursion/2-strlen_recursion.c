845174fe4e0251e28a73412323f585dde80b0fcc
Ace <acequantumbit@gmail.com>
2023-10-15 19:17:56 -0700

    appeasing betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - calls a recursive function
+ * @s: a pointer to the string to measure
+ * return: the length of the string
+ */
+
 int _strlen_recursion(char *s)
 {
 	int x = 0;
 
 	return (RecursiveStrLen(s, x));
 }
+
+/**
+ * RecursiveStrLen - recursively finds the strlen
+ * @s: string to find length of
+ * @x: the length of the string
+ */
 
 int RecursiveStrLen(char *s, int x)
 {
