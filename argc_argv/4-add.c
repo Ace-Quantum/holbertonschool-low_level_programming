506c95c8adc9ef2c1c687f5f3993593cdc6dfbae
Ace <acequantumbit@gmail.com>
2023-10-15 18:10:11 -0700

    let's just get past this

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 	int i;
 	int sum = 0;
 
-	if (argc = 1)
+	if (argc == 1)
 	{
 		printf("%d", 0);
 		return (0);
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 
 	for (i = 1; i <= argc; i++)
 	{
-		if (argv[i] < 4 || argv[i] > 57)
+		if (*argv[i] < 4 || *argv[i] > 57)
 		{
 			printf("Error");
 			return (1);
