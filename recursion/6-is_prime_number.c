08e42ee6336903da57363e701355913d2d13f02b
Ace <acequantumbit@gmail.com>
2023-10-15 20:24:52 -0700

    appeasing betty

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * is_prime_number - start of the programs
+ * @n: the number in question
+ * Return: 0 if not prime, 1 if so
+ */
+
 int is_prime_number(int n)
 {
 	int i;
@@ -11,6 +17,13 @@ int is_prime_number(int n)
 
 	return (rec_prime(n, i));
 }
+
+/**
+ * rec_prime - the actual calculations
+ * @n: the number in question
+ * @i: the iterator
+ * Return: 0 if not prime, 1 if so
+ */
 
 int rec_prime(int n, int i)
 {
