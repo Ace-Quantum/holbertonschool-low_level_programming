65d3bdd8e04b94d14c8883e7d26b2bf409b390e8
Ace <acequantumbit@gmail.com>
2023-10-19 16:54:30 -0700

    bug fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,14 +9,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(width * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		array[i] = malloc((height - 1) * sizeof(int));
+		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 			return (NULL);
 	}
