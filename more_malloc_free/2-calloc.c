0faf0372998efb1fb06fb4640ca95d6218719cc4
Ace <acequantumbit@gmail.com>
2023-10-21 18:51:35 -0700

    don't look at my comments

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,7 +7,15 @@ void *_calloc(unsigned int nmeb, unsigned int size)
 	int ssize = (int)size;
 	int i = 0;
 
-	char **array = (char **)malloc(nnmeb * sizeof(char *));
+	void *array = malloc(nmeb * size);
+
+	while (i < nnmeb)
+	{
+		array[i] = 0;
+		i++;
+	}
+
+	i = 0;
 
 	while (i < nnmeb)
 	{
@@ -15,13 +23,13 @@ void *_calloc(unsigned int nmeb, unsigned int size)
 		i++;
 	}
 
-	i = 0;
+	//i = 0;
 
-	while (i <nnmeb)
-	{
-		array[i] = 0;
-		i++;
-	}
+	//while (i <nnmeb)
+	//{
+		//array[i] = 0;
+		//i++;
+	//}
 
 	return (array);
 }
