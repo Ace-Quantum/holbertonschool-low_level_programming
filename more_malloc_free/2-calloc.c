6b6dea9b6be80d26a3b08230f9e71fae92077ef2
Ace <acequantumbit@gmail.com>
2023-10-22 14:42:21 -0700

    trying again

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,7 +7,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned char *array_start = (unsigned char *)array;
 	unsigned int i = 0;
 
-	while (i < size)
+	while (i < nmemb)
 	{
 		*array_start = 0;
 		array_start++;
