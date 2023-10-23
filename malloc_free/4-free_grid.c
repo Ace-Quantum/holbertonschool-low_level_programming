df4b7c08112f174a4347416f4b266dd1ab9cf365
Ace <acequantumbit@gmail.com>
2023-10-23 16:35:31 -0700

    appeasing betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a grid previously made earlier
+ * @grid: the grid in question
+ * @height: the height of the grid in question
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
+
+	if (grid = NULL)
+		return (void);
 
 	for (i = 0; i <= height; i++)
 		free(grid[i]);
 
 	free(grid);
+	return (void);
 }
