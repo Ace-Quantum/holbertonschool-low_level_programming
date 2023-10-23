3c43be44287810741a453dee53746b12f3197396
Ace <acequantumbit@gmail.com>
2023-10-23 15:51:04 -0700

    appeasing betty

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include "_strlen.c"
 
+/**
+ * _strdup - duplicates a string
+ * @str: string in question to duplicate
+ *
+ * Return: the newly made array to the newly duplicated string
+ */
+
 char *_strdup(char *str)
 {
 	char *array;
 	int i;
 
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 
 	array = malloc(_strlen(str) + 1);
 
 	if (array == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (i = 0; i < _strlen(str) + 1; i++)
 		array[i] = str[i];
