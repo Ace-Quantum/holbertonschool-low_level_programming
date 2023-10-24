39959c747a9d5c02398381e8d56da49ddcee18ea
Ace <acequantumbit@gmail.com>
2023-10-23 17:48:47 -0700

    appeasing betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * malloc_checked - checks for space to be allocated
+ * @b: the amount to check for
+ * Return: return a pointer to the allocated space
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
