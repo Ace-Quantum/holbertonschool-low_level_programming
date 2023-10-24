2c190b553e3a6afd391635f081465a011663c765
Ace <acequantumbit@gmail.com>
2023-10-23 18:11:57 -0700

    error handling

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -4,7 +4,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	void *ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
 
 	_memset(ptr, 0, nmemb * size);
 
