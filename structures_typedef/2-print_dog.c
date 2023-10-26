8d9c2c0ead16c2338d67c1bfcebbb8a464741c5c
Ace <acequantumbit@gmail.com>
2023-10-26 14:34:23 -0700

    still tripping over this

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,7 +11,9 @@ void print_dog(struct dog *d)
 		(*d).owner = "(nil)";
 
 	if (d != NULL)
+	{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
+	}
 }
