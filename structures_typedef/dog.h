31c60122f432c0419147a022be029771d289bdab
Ace <acequantumbit@gmail.com>
2023-10-26 15:01:55 -0700

    I still don't think it's going to be that easy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
