d398f8230ae66f483c922d16cf9a8d553338b513
Ace <AceQuantumBit@gmail.com>
2023-10-27 15:25:28 -0500

    accounting for nulls

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -7,6 +7,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
     int i = 0;
     int j = (int)size;
 
+    if (array == NULL || size == NULL)
+        return;
+    
     while (i < j)
     {
         (*action)(array[i]);
