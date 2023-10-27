27a6b24e3d2ddd98aa969a7d163561886bd8bb1b
Ace <AceQuantumBit@gmail.com>
2023-10-27 15:26:58 -0500

    bug fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -7,9 +7,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
     int i = 0;
     int j = (int)size;
 
-    if (array == NULL || size == NULL)
+    if (array == NULL || action == NULL)
         return;
-    
+
     while (i < j)
     {
         (*action)(array[i]);
