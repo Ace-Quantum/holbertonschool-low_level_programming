0df5bdcdefc472a4cbba44ae865e80af25d37b54
Ace <AceQuantumBit@gmail.com>
2023-10-29 01:17:42 -0500

    op_func bug

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     int i, result, a, b;
-    int (*op_func(*char))(int, int);
+    int *op_func(*char)(int, int);
 
     a = atoi(argv[1]);
     b = atoi(argv[3]);
