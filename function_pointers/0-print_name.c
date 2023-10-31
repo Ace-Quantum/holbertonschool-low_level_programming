8a8fff3a1effbbc1f324f0cfcadfd11fd3dfc116
Ace <AceQuantumBit@gmail.com>
2023-10-30 22:11:31 -0500

    Appeasing Betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+* print_name - calls a function to print a name
+*
+* @name: the name to print
+* @f: the function that prints
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
     if (*f)
