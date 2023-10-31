6f98bc96e66f5ed8c0b33eb7821d6ef32743a389
Ace <AceQuantumBit@gmail.com>
2023-10-30 22:25:14 -0500

    Appeasing betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* array_iterator - iterates through an array
+* @array: the array to iterate through
+* @size: the size of the array
+* @action: the function to call
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
