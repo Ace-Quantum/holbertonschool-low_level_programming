eb159288e44af657ac872c53b812bfe81fabe3e4
Ace <AceQuantumBit@gmail.com>
2023-10-30 21:53:11 -0500

    Appeasing Betty

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+* init_dog - beginning of the program
+* 
+* @d: the dogs
+* @name: name for dog
+* @age: age for dog
+* @owner: owner for the dog
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
