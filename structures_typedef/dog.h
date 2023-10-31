6e1bcf8e8bd482b5765912b722754c3453f785f3
Ace <AceQuantumBit@gmail.com>
2023-10-30 21:44:25 -0500

    Appeasing Betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef dog_h
+#define dog_h
+
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+* struct dog - the file for a dog
+* @name: Name of dog
+* @age: age of dog
+* @owner: Owner of dog
+*/
 typedef struct dog
 {
 	char *name;
@@ -19,3 +28,5 @@ int _strlen(char *s);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+
+#endif
