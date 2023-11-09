8b7232fb3107e3248765ca0bd04d9d9244af317f
Ace <AceQuantumBit@gmail.com>
2023-11-09 15:03:19 -0600

    made 0-print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t len;
+
+    len = 0;
+
+    while(h != NULL)
+    {
+        if ((*h).str == NULL)
+            printf("[%d] %s\n", 0, (*h).str);
+        else
+            printf("[%d] %s\n", (*h).len, (*h).str);
+        h = (*h).next
+    }
+    return (len);
+}
