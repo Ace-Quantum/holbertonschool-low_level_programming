56116aec257468cc36882d0a7b83bef1c7aa2b90
Ace <AceQuantumBit@gmail.com>
2023-11-09 15:05:11 -0600

    added semicolon

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
             printf("[%d] %s\n", 0, (*h).str);
         else
             printf("[%d] %s\n", (*h).len, (*h).str);
-        h = (*h).next
+        h = (*h).next;
     }
     return (len);
 }
