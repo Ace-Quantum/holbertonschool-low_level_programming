6d5161daf94fe069608bd5a92b5a7ddcd248bd23
Ace <AceQuantumBit@gmail.com>
2023-11-09 15:38:06 -0600

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,8 +7,13 @@ list_t *add_node(list_t **head, const char *str)
     list_t *new_node = NULL;
 
     new_node = malloc(sizeof(list_t));
+    
     if (new_node == NULL)
     {
-        return;
+        return (NULL);
     }
+
+    (*new_node).str = strdup(str);
+
+    return (&new_node);
 }
