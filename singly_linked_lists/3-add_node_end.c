d62e2f86ba01b9a870aa36953d7c631b0c8304c1
Ace <AceQuantumBit@gmail.com>
2023-11-10 14:20:54 -0600

    made add_node_end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node = NULL;
+    list_t *temp_node;
+
+    new_node = malloc(sizeof(list_t));
+
+    if (new_node == NULL);
+        return (NULL);
+
+    (*new_node).str = strdup(str);
+    (*new_node).str = strlen(str);
+    (*new_node).next = NULL;
+
+    *temp_node = *head;
+
+    while ((*temp_node).next != NULL)
+        *temp_node = (*temp_node).next;
+
+    (*temp_node).next = *new_node;
+
+    return(*new_node);
+}
