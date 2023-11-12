cb2a34040561172cbac4ad7360ffdc6f7b68325f
Ace <AceQuantumBit@gmail.com>
2023-11-11 20:41:45 -0600

    If you set the variable to a constant inside of a loop then you'll end up with an endless loop

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -12,7 +12,7 @@ void free_list(list_t *head)
 
     while (leader != NULL)
     {
-        leader = (*head).next;
+        leader = (*leader).next;
         free((*freer).str);
         /*free((*freer).len);*/
         free((*freer).next);
