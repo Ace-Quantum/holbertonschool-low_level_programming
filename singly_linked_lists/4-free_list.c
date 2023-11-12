07a2c6339779ac06f01f0788ee4602b8763201bb
Ace <AceQuantumBit@gmail.com>
2023-11-11 20:25:50 -0600

    made free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"
+#include "lists.h"
 
 void free_list(list_t *head)
 {
     list_t *leader;
-    list_t *freer = head;
+    list_t *freer;
 
+    if (head != NULL && (*head).next != NULL)
+    {
+        leader = head;
+        freer = head;
 
+        while (head != NULL)
+        {
+            leader = (*head).next;
+            free((*freer).str);
+            free((*freer).len);
+            free((*freer).next);
+            free(freer);
+            freer = leader;
+        }
+    }
 }
