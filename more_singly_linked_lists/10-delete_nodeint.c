7fcba7f373ebf6e590486275af0972449db9d396
Ace <AceQuantumBit@gmail.com>
2023-11-13 02:12:36 -0600

    and forgot to add one where it was

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
         }
         counter++;
         leader = (*leader).next;
-        freer = (*freer).next
+        freer = (*freer).next;
     }
     return (-1);
 }
