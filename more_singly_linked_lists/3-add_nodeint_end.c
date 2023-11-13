decb0817f6d7644d8497a06b37a237bca1de1a0d
Ace <AceQuantumBit@gmail.com>
2023-11-12 20:37:23 -0600

    made add_node_end

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    list_t *new_node = NULL;
+    list_t *temp_node;
+
+    new_node = malloc(sizeof(listint_t));
+
+    if (new_node == NULL)
+        return (NULL);
+
+    (*new_node).n = n;
+    (*new_node).next = NULL;
+
+    if (*head == NULL)
+        *head = new_node;
+    else
+    {
+        temp_node = *head;
+
+        while ((*temp_node).next != NULL)
+            temp_node = (*temp_node).next;
+
+        (*temp_node).next = new_node;
+    }
+
+    return(new_node);
+}
