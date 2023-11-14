656ce2fd6f00274d333e34247aca23e361705819
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:19:02 -0600

    betty

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_listint- start of program
+ * @h: head
+ * Return: number of nodes
+*/
 size_t print_listint(const listint_t *h)
 {
-    size_t counter;
+	size_t counter;
 
-    counter = 0;
+	counter = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", (*h).n);
-        h = (*h).next;
-        counter++;
-    }
-    return(counter);
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+		counter++;
+	}
+	return(counter);
 }
