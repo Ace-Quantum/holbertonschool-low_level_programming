10dfe716e90985a1c870b15f29a8a9006b82d416
Ace <AceQuantumBit@gmail.com>
2023-11-13 20:23:49 -0600

    betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * delete_node_at_index- Idk what they're wanting from me here
+ * @head: head
+ * @index: the index to delete
+ * Return: pass code or error code
+*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    /*if it were up to me these would be in the order of holder, freer, leader*/
-    /*but that's not the way the cookie crumbles today*/
-    /*lol nevermind that's exactly what it should be.*/
-    /*I gotta learn to trust my instincts more*/
-    listint_t *freer = *head;
-    listint_t *leader = (*freer).next;
+	listint_t *freer = *head;
+	listint_t *leader = (*freer).next;
 
-    unsigned int counter = 0;
+	unsigned int counter = 0;
 
-    while (leader != NULL)
-    {
-        if (counter == index)
-        {
-            (*freer).next = (*leader).next;
-            free(leader);
-            return(1);
-        }
-        counter++;
-        leader = (*leader).next;
-        freer = (*freer).next;
-    }
-    return (-1);
+	while (leader != NULL)
+	{
+		if (counter == index)
+		{
+			(*freer).next = (*leader).next;
+			free(leader);
+			return(1);
+		}
+		counter++;
+		leader = (*leader).next;
+		freer = (*freer).next;
+	}
+	return (-1);
 }
