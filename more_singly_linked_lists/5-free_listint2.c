94482563218760e92d2b801e8f62aa4ddd8238a4
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:29:11 -0600

    betty

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_listint2- free them
+ * @head: head
+*/
 void free_listint2(listint_t **head)
 {
-    listint_t *leader;
-    listint_t *freer = *head;
+	listint_t *leader;
+	listint_t *freer = *head;
 
-    if (*head == NULL)
-        return;
+	if (*head == NULL)
+		return;
 
-    while (freer != NULL)
-    {
-        leader = (*freer).next;
-        free(freer);
-        freer = leader;
-    }
+	while (freer != NULL)
+	{
+		leader = (*freer).next;
+		free(freer);
+		freer = leader;
+	}
 
-    *head = NULL;
+	*head = NULL;
 }
