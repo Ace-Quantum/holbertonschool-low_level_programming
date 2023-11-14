e62068040622cf400aa22c5ce696e16934fd3d51
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:41:45 -0600

    betty

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * sum_listint- this was the easiest one
+ * @head: why couldn't we start with this?
+ * Return: honestly idk what I'm going to do with everything else left
+*/
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *current = head;
+	int sum = 0;
+	listint_t *current = head;
 
-    while (current != NULL)
-    {
-        sum = sum + (*current).n;
-        current = (*current).next;
-    }
-    
-    return(sum);
+	while (current != NULL)
+	{
+		sum = sum + (*current).n;
+		current = (*current).next;
+	}
+	
+	return(sum);
 }
