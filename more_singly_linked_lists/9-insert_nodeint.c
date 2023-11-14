774ac0ac6d406d05fe25449a182f2f87d94df387
Ace <AceQuantumBit@gmail.com>
2023-11-13 20:13:19 -0600

    betty please be kind

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,46 +3,53 @@
 #include "lists.h"
 #include "2-add_nodeint.c"
 
+/**
+ * insert_nodeint_at_index- why does this one have to be so long?
+ * @head: head
+ * @idx: index to insert
+ * @n: the number for the node
+ * Return: the new node
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i = 0;
-    listint_t *new_node;
-    listint_t *tracker_node = *head;
+	unsigned int i = 0;
+	listint_t *new_node;
+	listint_t *tracker_node = *head;
 
-    new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-    if (head == NULL || *head == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (head == NULL || *head == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    (*new_node).n = n;
+	(*new_node).n = n;
 
-    if (idx == 0)
-    {
-        (*new_node).next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		(*new_node).next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    while (tracker_node != NULL && i < idx - 1)
-    {
-        i++;
-        tracker_node = (*tracker_node).next;
-    }
+	while (tracker_node != NULL && i < idx - 1)
+	{
+		i++;
+		tracker_node = (*tracker_node).next;
+	}
 
-    if (tracker_node == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (tracker_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    (*new_node).next = (*tracker_node).next;
-    (*tracker_node).next = new_node;
+	(*new_node).next = (*tracker_node).next;
+	(*tracker_node).next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
