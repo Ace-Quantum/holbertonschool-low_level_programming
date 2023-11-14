f51107cd5db8c4c97a6a41d1310681fb692d7b70
Ace <AceQuantumBit@gmail.com>
2023-11-13 18:42:37 -0600

    Trying my best here

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     {
         if (i == idx)
         {
-            (*new_node).next = (*tracker_node).next;
-            (*tracker_node).next = new_node;
+            (*new_node).next = *tracker_node;
             return (new_node);
         }
         i++;
