217ec2494791e0731904a7ce152f80a7bdb0826e
Ace <AceQuantumBit@gmail.com>
2023-11-13 20:16:52 -0600

    honestly that's what I thought was happening but that's what I get I guess

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,12 +18,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	/*if (head == NULL || *head == NULL)
-	{
-		free(new_node);
-		return (*head);
-	}*/
-
 	if (new_node == NULL)
 		return (*head);
 
