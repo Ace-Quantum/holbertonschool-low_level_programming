a2ae23c13fc80b0934270793f9a284d26074c890
Ace <AceQuantumBit@gmail.com>
2023-11-13 20:25:38 -0600

    betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * delete_node_at_index- Idk what they're wanting from me here
+ * delete_nodeint_at_index- Idk what they're wanting from me here
  * @head: head
  * @index: the index to delete
  * Return: pass code or error code
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			(*freer).next = (*leader).next;
 			free(leader);
-			return(1);
+			return (1);
 		}
 		counter++;
 		leader = (*leader).next;
