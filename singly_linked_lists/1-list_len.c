b5c5d37c90e7b55d9f96d25818d1764f7a9d19fe
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:09:59 -0600

    Betty c'mon be nice

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - starting point of program
+ *
+ * @h: head
+ * Return: the number of nodes
+*/
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while(h != NULL)
-    {
-        count++;
-        h = (*h).next;
-    }
-    return (count);
+	while(h != NULL)
+	{
+		count++;
+		h = (*h).next;
+	}
+	return (count);
 }
