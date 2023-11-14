160d87ee9448af75a897828c4c4a4174e2ffc9fd
Ace <AceQuantumBit@gmail.com>
2023-11-14 16:57:31 -0600

    betty & comp error

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
     if (b == NULL)
         return (0);
 
-    while (*b != NULL)
+    while (b != NULL)
     {
         if (*b != '0' && *b != '1')
             return (0);
