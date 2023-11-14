5b9c39590bc9d73abde5da5d0b836ca99c6f1654
Ace <AceQuantumBit@gmail.com>
2023-11-13 18:10:04 -0600

    made get nodeint

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "lists.h"
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    
+    unsigned int i = 0;
+    listint_t *tracker_node = head;
+
+    while (tracker_node != NULL)
+    {
+        if (i == index)
+            return (tracker_node);
+        tracker_node = (*tracker_node).next;
+        i++;
+    }
+
+    return (NULL);
 }
