2ae45de978329aee817d503a838c3e0e58f93372
Ace <AceQuantumBit@gmail.com>
2023-11-13 18:26:23 -0600

    made insert_node

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+    unsigned int i = 0;
+    listint_t *new_node;
+    listint_t *tracker_node = *head;
+
+    new_node = malloc(sizeof(listint_t));
+
+    if new_node = NULL
+        return (NULL);
+
+    (*new_node).n = n;
+
+    while (tracker_node != NULL)
+    {
+        if (i = idx)
+        {
+            (*new_node).next = (*tracker_node).next;
+            (*tracker_node).next = new_node;
+            return (new_node);
+        }
+        i++;
+        tracker_node = (*tracker_node).next;
+    }
+    return (NULL);
+}
