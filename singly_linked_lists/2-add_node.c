23c0b19ed71ad356c57025f3b1480a1f37d8e1e8
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:12:51 -0600

    no thoughts, just betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,22 +3,31 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * add_node- start of code
+ *
+ * @head: head
+ * @str: str
+ *
+ * Return: new head
+*/
+
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = NULL;
+	list_t *new_node = NULL;
 
-    new_node = malloc(sizeof(list_t));
-    
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	new_node = malloc(sizeof(list_t));
+	
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    (*new_node).str = strdup(str);
-    (*new_node).len = strlen(str);
+	(*new_node).str = strdup(str);
+	(*new_node).len = strlen(str);
 
-    (*new_node).next = *head;
-    *head = new_node;
+	(*new_node).next = *head;
+	*head = new_node;
 
-    return (*head);
+	return (*head);
 }
