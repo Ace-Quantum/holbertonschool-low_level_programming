182c72e88d26a3951b5ec6140529faa22b595696
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:21:42 -0600

    betty

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * listint_len- start of program
+ * @h: head
+ * Return: the slab
+*/
 size_t listint_len(const listint_t *h)
 {
-    size_t counter;
+	size_t counter;
 
-    counter = 0;
+	counter = 0;
 
-    while (h != NULL)
-    {
-        counter++;
-        h = (*h).next;
-    }
-    return(counter);
+	while (h != NULL)
+	{
+		counter++;
+		h = (*h).next;
+	}
+	return (counter);
 }
