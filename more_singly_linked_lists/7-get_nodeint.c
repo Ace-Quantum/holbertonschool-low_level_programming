80f4f182d8da24d9f74f9386da54888af0e95947
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:40:12 -0600

    honestly no one can hear me scream and let's keep it that way

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index- there's no reason it should be that long
+ * @head: head
+ * @index: the indexed node to return
+ * Return: the node in question
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    listint_t *tracker_node = head;
+	unsigned int i = 0;
+	listint_t *tracker_node = head;
 
-    while (tracker_node != NULL)
-    {
-        if (i == index)
-            return (tracker_node);
-        tracker_node = (*tracker_node).next;
-        i++;
-    }
+	while (tracker_node != NULL)
+	{
+		if (i == index)
+			return (tracker_node);
+		tracker_node = (*tracker_node).next;
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
