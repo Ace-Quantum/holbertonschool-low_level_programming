ade05ee52a0a9c2cd8760692cd709e63aab65357
Ace <AceQuantumBit@gmail.com>
2023-11-14 16:54:46 -0600

    Okidoke I'm feeling this

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * binary_to_uint- take a string of ones and zeros and make it into an integer
+ * @b: the string of ones and zeros to calculate out
+ * Return: the number in base 10
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int i;
+
+    if (b == NULL)
+        return (0);
+
+    while (*b != NULL)
+    {
+        if (*b != '0' && *b != '1')
+            return (0);
+
+        i = ((i << 1) | (*b + '0'));
+        b++;
+    }
+
+    return (i);
+}
