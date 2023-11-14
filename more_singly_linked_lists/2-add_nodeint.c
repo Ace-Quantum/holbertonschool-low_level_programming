be0457686fb585852783dd67cfd22aa6ec27662d
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:23:16 -0600

    betty

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * add_nodeint- start
+ * @head: head
+ * @n: number
+ * Return: the slab
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new_node = NULL;
+	listint_t *new_node = NULL;
 
-    new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    (*new_node).n = n;
+	(*new_node).n = n;
 
-    (*new_node).next = *head;
-    *head = new_node;
+	(*new_node).next = *head;
+	*head = new_node;
 
-    return(*head);
+	return (*head);
 }
