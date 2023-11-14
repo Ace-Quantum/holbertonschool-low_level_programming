6fae2fe9b08241079a11ef7ed945e68aa84225a0
Ace <AceQuantumBit@gmail.com>
2023-11-13 19:15:18 -0600

    betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,31 +3,39 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * add_node_end - start of program
+ *
+ * @head: head
+ * @str: str
+ *
+ * Return: the new node added
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node = NULL;
-    list_t *temp_node;
+	list_t *new_node = NULL;
+	list_t *temp_node;
 
-    new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    (*new_node).str = strdup(str);
-    (*new_node).len = strlen(str);
-    (*new_node).next = NULL;
+	(*new_node).str = strdup(str);
+	(*new_node).len = strlen(str);
+	(*new_node).next = NULL;
 
-    if (*head == NULL)
-        *head = new_node;
-    else
-    {
-        temp_node = *head;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		temp_node = *head;
 
-        while ((*temp_node).next != NULL)
-            temp_node = (*temp_node).next;
+		while ((*temp_node).next != NULL)
+			temp_node = (*temp_node).next;
 
-        (*temp_node).next = new_node;
-    }
+		(*temp_node).next = new_node;
+	}
 
-    return(new_node);
+	return(new_node);
 }
