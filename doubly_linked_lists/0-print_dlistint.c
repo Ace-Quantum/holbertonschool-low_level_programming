4bfc3f7e6189fbb251d3fca21563c7432ac0c747
Ace <AceQuantumBit@gmail.com>
2023-11-15 10:07:37 -0600

    made header, readme, and print list function

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * Betty here
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t len = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", (*h).n);
+        h = (*h).next;
+        len++;
+    }
+    return (counter);
+}
