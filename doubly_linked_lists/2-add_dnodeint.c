6c037b294d0782ced271e5011b210f892c57567d
Ace <AceQuantumBit@gmail.com>
2023-11-15 10:39:24 -0600

    built a function to add a node to a doubly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * Betty lies here
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new_node = NULL;
+
+    new_node = malloc(sizeof(listint_t));
+
+    if (new_node == NULL)
+        return (NULL);
+
+    (*new_node).n = n;
+
+    (*new_node).next = *head;
+    (*new_node).prev = NULL
+    *head = new_node;
+
+    return (*head);
+}
