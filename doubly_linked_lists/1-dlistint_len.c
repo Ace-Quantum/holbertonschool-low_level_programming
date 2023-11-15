8bd85c97d37548786fc4d0b48a7d8182bf61d806
Ace <AceQuantumBit@gmail.com>
2023-11-15 10:24:18 -0600

    made function to return number of nodes

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * Betty goes here
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t num_nodes = 0;
+
+    while (h != NULL)
+    {
+        num_nodes++;
+        h = (*h).next
+    }
+    return (num_nodes);
+}
