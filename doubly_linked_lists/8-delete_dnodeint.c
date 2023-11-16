47b55e773d003750b9f66f3ea852944bf8ab4328
Ace <AceQuantumBit@gmail.com>
2023-11-16 16:17:26 -0600

    made delete node at index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *freer = NULL;
+    dlistint_t *leader = NULL;
+
+    unsigned int counter = 0;
+
+    if (*head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        freer = *head;
+        *head = (**head).next;
+        (*head)->prev = NULL;
+        free(freer);
+        return (1);
+    }
+
+    leader = *head;
+
+    while (counter != index - 1)
+    {
+        leader = (*leader).next;
+        counter++;
+    }
+
+    freer = (*leader).next;
+
+    if ((*freer).next != NULL)
+    {
+        (*leader).next = (*freer).next;
+        leader->next->prev = leader
+    }
+    else
+        leader->next = NULL;
+
+    free(freer);
+    return (1);
+}
