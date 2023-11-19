e8146da712aabc35e9af18594875d7a23fac128d
Ace <AceQuantumBit@gmail.com>
2023-11-18 19:32:20 -0600

    comp fixes

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,7 +4,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = hash_djb2(key);
+    unsigned long int hash = hash_djb2(key);
 
     return (hash % size);
 }
