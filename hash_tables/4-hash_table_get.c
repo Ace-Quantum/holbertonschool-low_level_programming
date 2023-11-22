4714ea389d6a70f3646e0c355dd7f0a69ddeac1a
Ace <AceQuantumBit@gmail.com>
2023-11-22 12:32:17 -0600

    added loop

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,5 +11,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
     temp_node = ht->array[hash_key];
 
-    return (temp_node->value);
+    while (temp_node != NULL)
+    {
+        if (temp_node->key == key)
+            return (temp_node->value);
+        temp_node = temp_node->next;
+    }
+
+    return (NULL);
 }
