cc31f2ff7205512042cf28b07ac78441e93bf3d6
Ace <AceQuantumBit@gmail.com>
2023-11-22 12:38:06 -0600

    fixed newline

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,11 +12,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
     temp_node = ht->array[hash_key];
 
-    printf("key: %s, val: %s/n", temp_node->key, temp_node->value);
+    printf("key: %s, val: %s\n", temp_node->key, temp_node->value);
 
     while (temp_node != NULL)
     {
-        printf("%d iteration: key: %s, val %s/n", i, temp_node->key, temp_node->value);
+        printf("%d iteration: key: %s, val %s\n", i, temp_node->key, temp_node->value);
         i++;
         if (temp_node->key == key)
             return (temp_node->value);
