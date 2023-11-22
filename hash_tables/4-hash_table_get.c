acb5cd8ea83f127542ec82363bf30aec0947c3cb
Ace <AceQuantumBit@gmail.com>
2023-11-22 10:14:55 -0600

    I'll eventually get the hang of pointers

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,6 +13,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     {
         if (tracker->key == key)
             return (tracker->value);
-        *tracker++;
+        tracker++;
     }
 }
