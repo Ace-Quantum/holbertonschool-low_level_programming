278a938dc300650741c531638e957fd3e3382f48
Ace <AceQuantumBit@gmail.com>
2023-11-22 10:05:46 -0600

    Honestly it might actually be this simple

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    /**unsigned long int hash_val;**/
+    hash_node_t *tracker = ht->array;
+
+    /**hash_val = key_index((const unsigned char *)key, ht->size);**/
+
+    while (*tracker)
+    {
+        if (tracker->key == key)
+            return (tracker->value);
+        *tracker++;
+    }
+}
