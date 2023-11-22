55c40b4aee19b52ea0f6f4607ff80351ec1cfb4f
Ace <AceQuantumBit@gmail.com>
2023-11-22 15:37:45 -0600

    betty 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,11 +13,11 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int hash_key;
-	hash_node_t* temp_node;
+	hash_node_t *temp_node;
 	/**int i = 0;**/
 
 	if (!ht || !key)
-		return(NULL);
+		return (NULL);
 
 	hash_key = key_index((const unsigned char *)key, ht->size);
 
