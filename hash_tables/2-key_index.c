2bc588a923f471c51bb8570cd88f28886c03b20e
Ace <AceQuantumBit@gmail.com>
2023-11-22 15:32:55 -0600

    return betty 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,9 +3,10 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gets an index from a key
+ * key_index- gets an index from a key
  * @key: the key to get the index from
  * @size: the size of the hash table
+ * Return: the hash value
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
