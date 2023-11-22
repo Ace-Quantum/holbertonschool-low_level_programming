fa3bf7616c062bccf6d119ce6cc4890a8155a0ca
Ace <AceQuantumBit@gmail.com>
2023-11-22 12:36:23 -0600

    more debuggers

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -6,15 +6,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
     unsigned long int hash_key;
     hash_node_t* temp_node;
+    int i = 0;
 
     hash_key = key_index((const unsigned char *)key, ht->size);
 
     temp_node = ht->array[hash_key];
 
-    printf("key: %s, val: %s", temp_node->key, temp_node->value);
-    
+    printf("key: %s, val: %s/n", temp_node->key, temp_node->value);
+
     while (temp_node != NULL)
     {
+        printf("%d iteration: key: %s, val %s/n", temp_node->key, temp_node->value);
+        i++;
         if (temp_node->key == key)
             return (temp_node->value);
         temp_node = temp_node->next;
