a94d270f1a8814b56be64b63c254db13b7931860
Ace <AceQuantumBit@gmail.com>
2023-11-22 14:04:25 -0600

    I know there's a bug but I'm not sure how to fix it up

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,5 +4,20 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t
+    hash_node_t* temp_node;
+    int i;
+
+    for (i == 0; i <= ht->size; i++)
+    {
+        if (ht->array[i] != NULL)
+        {
+            temp_node = ht->array[i];
+            while (temp_node != NULL)
+            {
+                printf("'%s': '%s', ", temp_node->key, temp_node->value);
+                temp_node = temp_node->next
+            }
+        }
+    }
+    return (void);
 }
