abcc9a47e8ccb213d646c3d09fbd4a55c5a83c6a
Ace <AceQuantumBit@gmail.com>
2023-12-22 14:07:54 -0600

    bettiskabetti

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,17 +3,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t return_index = 0;
+	size_t index = 0;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (return_index < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n", return_index, array[return_index]);
-        if (array[return_index] == value)
-            return(return_index);
-        return_index++;
-    }
-    return (-1);
+	while (index < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+		index++;
+	}
+	return (-1);
 }
