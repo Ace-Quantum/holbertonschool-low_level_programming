1e11931624b6868e5fb812ae38dfa280d8628806
Ace <AceQuantumBit@gmail.com>
2023-12-22 12:27:12 -0600

    Okay so now we know

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,7 +10,7 @@ int binary_search(int *array, size_t size, int value)
 
     while (minidx <= maxidx)
     {
-        idx = maxidx / 2;
+        idx = minidx + (maxidx - minidx) / 2;
         printf("Searching in array: ");
 
         for (i = minidx; i <= maxidx; i++)
