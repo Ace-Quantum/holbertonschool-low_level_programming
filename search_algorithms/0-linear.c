8c168f06f7c4ec75d0f7cabd4e0795dd475db212
Ace <AceQuantumBit@gmail.com>
2023-12-22 12:32:04 -0600

    It's actually that simple right?

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,11 +3,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int return_index;
+    size_t return_index = 0;
 
     if (array == NULL)
         return (-1);
 
-    
-
+    while (return_index < size)
+    {
+        printf("Value checked array[%lu] = [%d]\n", return_index, array[return_index]);
+        if (array[return_index] == value)
+            return(return_index);
+    }
+    return (-1);
 }
