8dc63991bed95ec4240c654f2dabf4a1cdd2fd67
Ace <AceQuantumBit@gmail.com>
2023-12-22 14:12:07 -0600

    habetty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "search_algos.h"
 
+/**
+ * linear_search - searches an array linearly
+ * 
+ * @array: the array to search through
+ * @size: the size of the array
+ * @value: the value looking for
+ * 
+ * Return: the index with the array's val that we want
+*/
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t index = 0;
